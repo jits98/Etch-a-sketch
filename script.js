e01f85fe0e2fedbb8efebfde1452c2f14d40d0ad
jits98 <jits8809@gmail.com>
2025-09-27 09:40:02 +0530

    add prompt for user input

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const numberOfDivs = 16;
+let numberOfDivs = 16;
 
-// const userInput = prompt(
-//   "How many divs do you want but it shouldn't be more than 100?",
-//   ""
-// );
+const button = document.querySelector(".button");
+
+function inputValue() {
+  let promptValue;
+  while (true) {
+    promptValue = prompt(
+      "How many divs do you want? Please enter a number between 1 and 100:",
+      ""
+    );
+
+    if (promptValue === null) {
+      alert("Input cancelled.");
+      return null;
+    }
+
+    let num = Number(promptValue.trim());
+
+    if (!isNaN(num) && num >= 1 && num <= 100) {
+      num = numberOfDivs;
+      return numberOfDivs;
+    } else {
+      alert("Invalid input. Please enter a number between 1 and 100");
+    }
+  }
+}
+
+button.addEventListener("click", inputValue);
+
 const parentContainer = document.querySelector(".parent-container");
 
 function creatingRowDivs() {
